31dbeb8f67519a91ccf00f10843d214edb5fdeb6
DharunRaju005 <dharunraju22@gmail.com>
2024-01-31 23:05:45 +0530

    added signOut functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import './App.css';
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           {/* Use the `element` prop instead of `children` */}
           <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
     </div>
